02edad4eacc7ad5e441294bb9840ca84774b65f8
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-08-19 23:55:23 -0400

    Update BinarySearch.java

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,20 +1,23 @@
 public class BinarySearch {
     public static void main(String[] args) {
-        int[]arr={-2,-1,0,3,4,8,10,12};
-        int target=15;
-        int ans=binarySearch(arr,target);
-        System.out.println(ans);
+        int[] arr = {-2, -1, 0, 3, 4, 8, 10, 12};
+        int target = 15;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans); 
     }
-    public static int binarySearch(int arr[],int target){
-        int left=0;
-        int right=arr.length-1;
-        while(left<=right){
-            int mid=(left+right)/2;
-            if(target==arr[mid]) return mid;
-            else if (target<=arr[mid]) {
-                right=mid-1;
-            } else if (target>=arr[mid]) {
-                left=mid+1;
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2; 
+
+            if (target == arr[mid]) {
+                return mid;
+            } else if (target < arr[mid]) {
+                right = mid - 1; 
+            } else {
+                left = mid + 1; 
             }
         }
         return -1;
