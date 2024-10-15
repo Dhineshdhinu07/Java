81612ad37f270ffae2e96e212203e009a1b39bd5
Dhineshdhinu07 <dhinesh1278@gmail.com>
2024-10-15 15:11:18 +0530

    initial commit

--- src/Linkedlist/MainLinkedList.java
+++ src/Linkedlist/MainLinkedList.java
@@ -16,16 +16,22 @@ public class MainLinkedList {
 //        System.out.println(list.delete(2));
 //
 //        list.display();
-        CustomDLL list = new CustomDLL();
-        list.insertFirst(6);
-        list.insertFirst(5);
-        list.insertFirst(4);
-        list.insertFirst(3);
-        list.insertFirst(2);
-        list.insertFirst(1);
-        list.insertLast(7);
-        list.insert(7, 8);
-        list.insert(3, 0);
+//        CustomDLL list = new CustomDLL();
+//        list.insertFirst(6);
+//        list.insertFirst(5);
+//        list.insertFirst(4);
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(1);
+//        list.insertLast(7);
+//        list.insert(7, 8);
+//        list.insert(3, 0);
+//        list.display();
+        CustomCLL list = new CustomCLL();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.delete(1);
         list.display();
 
 
