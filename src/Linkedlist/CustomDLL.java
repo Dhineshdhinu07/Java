c7ec3bb42085b7d46bf1d6b43fe1851e4a8f7214
Dhineshdhinu07 <dhinesh1278@gmail.com>
2024-10-15 11:23:50 +0530

    initial commit

---
+++ src/Linkedlist/CustomDLL.java
@@ -0,0 +1,93 @@
+package Linkedlist;
+
+public class CustomDLL {
+    Node head;
+
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        node.prev = null;
+        if(head != null){
+            head.prev = node;
+        }
+        head = node;
+    }
+    public void display(){
+        Node node = head;
+        Node last = null;
+
+        while (node != null){
+            System.out.print(node.val + " -> ");
+            last = node;
+            node = node.next;
+        }
+        System.out.print("NULL");
+        System.out.println();
+        System.out.println("REVERSE OF LINKED LIST");
+        while (last!=null){
+            System.out.print(last.val + " -> ");
+            last = last.prev;
+        }
+        System.out.print("NULL");
+    }
+
+    public void insertLast(int val){
+        Node node = new Node(val);
+        Node last = head;
+        node.next = null;
+        if(head == null){
+            node.prev = null;
+            head = node;
+            return;
+        }
+        while (last.next != null){
+            last = last.next;
+        }
+            last.next = node;
+            node.prev = last;
+    }
+    public Node find(int value){
+        Node node = head;
+        while(node != null){
+            if (node.val == value){
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    public void insert(int after, int val){
+        Node p = find(after); //Here p is the node which is before the node to be inserted
+        if(p == null){
+            System.out.println("Node doesn't exists");
+            return;
+        }
+        Node node = new Node(val);
+        node.next = p.next;
+        p.next = node;
+        node.prev = p;
+        if(node.next != null){
+            node.next.prev = node;
+        }
+
+    }
+
+
+
+    private class Node{
+        int val;
+        Node next;
+        Node prev;
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node prev, Node next) {
+            this.val = val;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+}
--- src/Linkedlist/MainLinkedList.java
+++ src/Linkedlist/MainLinkedList.java
@@ -2,20 +2,32 @@ package Linkedlist;
 
 public class MainLinkedList {
     public static void main(String[] args) {
-        CustomLinkedList list = new CustomLinkedList();
+//        CustomLinkedList list = new CustomLinkedList();
+//        list.insertFirst(5);
+//        list.insertFirst(4);
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(1);
+//        list.insertLast(6);
+//        list.insertLast(7);
+//        list.insert(0,0);
+//        System.out.println(list.deleteFirst());
+//        System.out.println(list.deleteLast());
+//        System.out.println(list.delete(2));
+//
+//        list.display();
+        CustomDLL list = new CustomDLL();
+        list.insertFirst(6);
         list.insertFirst(5);
         list.insertFirst(4);
         list.insertFirst(3);
         list.insertFirst(2);
         list.insertFirst(1);
-        list.insertLast(6);
         list.insertLast(7);
-        list.insert(0,0);
-        System.out.println(list.deleteFirst());
-        System.out.println(list.deleteLast());
-        System.out.println(list.delete(2));
-
+        list.insert(7, 8);
+        list.insert(3, 0);
         list.display();
+
 
     }
 }
